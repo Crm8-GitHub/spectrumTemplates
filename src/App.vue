225d8c47e3389e4b65cad6201eb89327f8cd4a04
david07matthew-theo <david.07.matthew@gmail.com>
2026-02-05 10:37:59 +0530

    Second Commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-
+    <SPISiteSurvey/>
   </div>
 </template>
 
 <script setup>
-
+import SPISiteSurvey from './components/SPI Site Survey.vue';
 </script>
 
 <style lang="scss" scoped>
