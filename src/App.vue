1db4ac1c4d992fc619240af921fc11cb8925795f
david07matthew-theo <david.07.matthew@gmail.com>
2026-02-06 18:28:29 +0530

    External Pannel Commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,124 @@
 <template>
-  <div>
-    <SPISiteSurvey/>
+  <div class=" bg-[#eef1f9] h-screen print:h-auto w-screen flex flex-col justify-center items-center">
+    <div class=" h-17 w-full bg-white flex justify-between items-center px-2.5 py-3 box-border border-b border-b-[#dcdbee] print:hidden!">
+      <div class=" w-3/5 pl-7 font-puvi-medium box-border text-lg overflow-hidden text-nowrap text-ellipsis" title="Al Mahina LLC Building Dubai, 87654">
+        Al Mahina LLC Building Dubaiy, 87654 
+      </div>
+      <div class=" w-2/5 flex justify-end items-center">
+        <a href="" target="_blank" title="Download" class=" h-8 text-sm px-4 py-1 mr-2 shadow-[inset_0_-1px_0_#2b42a9] font-puvi-regular bg-primary-linearGradient text-black border border-[#d0d4df]  rounded-md box-border flex justify-between items-center gap-x-1.5 ">
+          <svg class=" pb-px" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32">
+            <path fill="none" stroke="#ffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 22V5M9 16l7 7l7-7M9 27h14"/>
+          </svg>
+          <div class=" text-white">
+            Download
+          </div>
+        </a>
+        <a href="" target="_blank" title="Print" class=" h-8 text-sm px-5 py-1 mr-2  shadow-[inset_0_-1px_0_#cdd1e6] font-puvi-regular bg-default-linearGradient text-black border border-[#d0d4df]  rounded-md box-border">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+            <path fill="#000000" d="M7 9.5a1.5 1.5 0 1 0 0 3a1.5 1.5 0 0 0 0-3zm0 2a.5.5 0 1 1 0-1a.5.5 0 0 1 0 1zM19.5 6H18V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5V6H4.5A2.502 2.502 0 0 0 2 8.5V15a3.003 3.003 0 0 0 3 3h1v3.5a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 .5-.5V18h1a3.003 3.003 0 0 0 3-3V8.5A2.502 2.502 0 0 0 19.5 6zM7 3h10v3H7V3zm10 18H7v-6h10v6zm4-6a2.003 2.003 0 0 1-2 2h-1v-2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5V17H5a2.003 2.003 0 0 1-2-2V8.5A1.5 1.5 0 0 1 4.5 7h15A1.5 1.5 0 0 1 21 8.5V15z"/>
+          </svg>
+        </a>
+        <a href="" target="_blank" title="Edit Template" class=" h-8 text-sm px-5 py-1 mr-2  shadow-[inset_0_-1px_0_#cdd1e6] font-puvi-regular bg-default-linearGradient text-black border border-[#d0d4df]  rounded-md box-border">
+          Edit
+        </a>
+      </div>
+    </div>
+
+    <div class="h-[calc(100%-60px)] print:h-auto w-full overflow-y-scroll print:overflow-hidden flex flex-col items-center py-5 print:py-0">
+      <div class=" h-auto print:w-screen! shadow-xl shadow-slate-350">
+        <div class=" w-225 print:w-screen bg-white px-5 box-border">
+          <table class=" w-full">
+            <thead class=" table-header-group!">
+              <tr>
+                <td>
+                  <div class=" h-30 w-full">
+                    <div class=" h-25 mt-5 bg-red-400">
+
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <table border="1" cellpadding="8" cellspacing="0" style="border-collapse: collapse; width: 100%;">
+                    <thead>
+                      <tr>
+                        <th>Column 1</th>
+                        <th>Column 2</th>
+                        <th>Column 3</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr><td>Row 1</td><td>Data 1</td><td>Value 1</td></tr>
+                      <tr><td>Row 2</td><td>Data 2</td><td>Value 2</td></tr>
+                      <tr><td>Row 3</td><td>Data 3</td><td>Value 3</td></tr>
+                      <tr><td>Row 4</td><td>Data 4</td><td>Value 4</td></tr>
+                      <tr><td>Row 5</td><td>Data 5</td><td>Value 5</td></tr>
+                      <tr><td>Row 6</td><td>Data 6</td><td>Value 6</td></tr>
+                      <tr><td>Row 7</td><td>Data 7</td><td>Value 7</td></tr>
+                      <tr><td>Row 8</td><td>Data 8</td><td>Value 8</td></tr>
+                      <tr><td>Row 9</td><td>Data 9</td><td>Value 9</td></tr>
+                      <tr><td>Row 10</td><td>Data 10</td><td>Value 10</td></tr>
+
+                      <tr><td>Row 11</td><td>Data 11</td><td>Value 11</td></tr>
+                      <tr><td>Row 12</td><td>Data 12</td><td>Value 12</td></tr>
+                      <tr><td>Row 13</td><td>Data 13</td><td>Value 13</td></tr>
+                      <tr><td>Row 14</td><td>Data 14</td><td>Value 14</td></tr>
+                      <tr><td>Row 15</td><td>Data 15</td><td>Value 15</td></tr>
+                      <tr><td>Row 16</td><td>Data 16</td><td>Value 16</td></tr>
+                      <tr><td>Row 17</td><td>Data 17</td><td>Value 17</td></tr>
+                      <tr><td>Row 18</td><td>Data 18</td><td>Value 18</td></tr>
+                      <tr><td>Row 19</td><td>Data 19</td><td>Value 19</td></tr>
+                      <tr><td>Row 20</td><td>Data 20</td><td>Value 20</td></tr>
+
+                      <tr><td>Row 21</td><td>Data 21</td><td>Value 21</td></tr>
+                      <tr><td>Row 22</td><td>Data 22</td><td>Value 22</td></tr>
+                      <tr><td>Row 23</td><td>Data 23</td><td>Value 23</td></tr>
+                      <tr><td>Row 24</td><td>Data 24</td><td>Value 24</td></tr>
+                      <tr><td>Row 25</td><td>Data 25</td><td>Value 25</td></tr>
+                      <tr><td>Row 26</td><td>Data 26</td><td>Value 26</td></tr>
+                      <tr><td>Row 27</td><td>Data 27</td><td>Value 27</td></tr>
+                      <tr><td>Row 28</td><td>Data 28</td><td>Value 28</td></tr>
+                      <tr><td>Row 29</td><td>Data 29</td><td>Value 29</td></tr>
+                      <tr><td>Row 30</td><td>Data 30</td><td>Value 30</td></tr>
+
+                      <tr><td>Row 31</td><td>Data 31</td><td>Value 31</td></tr>
+                      <tr><td>Row 32</td><td>Data 32</td><td>Value 32</td></tr>
+                      <tr><td>Row 33</td><td>Data 33</td><td>Value 33</td></tr>
+                      <tr><td>Row 34</td><td>Data 34</td><td>Value 34</td></tr>
+                      <tr><td>Row 35</td><td>Data 35</td><td>Value 35</td></tr>
+                      <tr><td>Row 36</td><td>Data 36</td><td>Value 36</td></tr>
+                      <tr><td>Row 37</td><td>Data 37</td><td>Value 37</td></tr>
+                      <tr><td>Row 38</td><td>Data 38</td><td>Value 38</td></tr>
+                      <tr><td>Row 39</td><td>Data 39</td><td>Value 39</td></tr>
+                      <tr><td>Row 40</td><td>Data 40</td><td>Value 40</td></tr>
+
+                      <tr><td>Row 41</td><td>Data 41</td><td>Value 41</td></tr>
+                      <tr><td>Row 42</td><td>Data 42</td><td>Value 42</td></tr>
+                      <tr><td>Row 43</td><td>Data 43</td><td>Value 43</td></tr>
+                      <tr><td>Row 44</td><td>Data 44</td><td>Value 44</td></tr>
+                      <tr><td>Row 45</td><td>Data 45</td><td>Value 45</td></tr>
+                    </tbody>
+                  </table>
+
+                </td>
+              </tr>
+            </tbody>
+            <tfoot class=" table-footer-group!">
+              <tr>
+                <td>
+                  <div class=" h-12 mb-5 border-t border-t-gray-300 align-baseline text-right">
+                    Form 90/7365D/-0098
+                  </div>
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,6 +126,13 @@
 import SPISiteSurvey from './components/SPI Site Survey.vue';
 </script>
 
-<style lang="scss" scoped>
-
+<style >
+.bg-primary-linearGradient
+{
+  background: transparent linear-gradient(0deg, #3954CC 0%, #5F7CFC 100%);
+}
+.bg-default-linearGradient
+{
+  background: transparent linear-gradient(180deg,#FCFCFF 1.4%,#EBEAF5 100%);
+}
 </style>
--- src/components/SPI Site Survey.vue
+++ src/components/SPI Site Survey.vue
@@ -1,7 +1,26 @@
 <template>
-    <div>
-        Hello world!
-    </div>
+    <table class=" w-full border-collapse border border-gray-800 ">
+        <thead class=" table-header-group!">
+            <tr>
+                <td>
+                    <div class=" w-full h-25 flex justify-around items-center">
+                        <div class=" w-1/5">
+                            <img src="https://spectruminternational.ae/assets/Images/Logo.png" 
+                            class=" h-16 "
+                            alt="Spectrum Inter">
+                        </div>
+                        <div class=" w-2/5">
+                            SITE SURVEY FORM (AMC)
+                        </div>
+                        <div class=" w-2/5">
+                            SPECTRUM INTERNATIONAL LLC <br>
+                            admin@spectruminternational.ae
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        </thead>
+    </table>
 </template>
 
 <script setup>
